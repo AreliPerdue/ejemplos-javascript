bf9bb609e798edd990a8e55ac51dc661a068b81a
Areli Perdue <perdue.areli28@gmail.com>
2026-01-21 14:27:27 -0600

    Implementing button operations

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,31 @@
 //alert("hola mundo"); /*sirve para poner un mensaje pop-up cuando se abre la pagina*/
 
-function Mostrar () {
+function mostrar (e) {
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
-
     let resultado = 0;
-    
-    resultado = numero1 + numero2;
+    let operacion = e.target.id;
 
-    let objResultado= document.getElementById("resultado");
-    objResultado.innerHTML = resultado;
+    if(operacion == "suma") {
+        resultado = numero1 + numero2;
+    } 
+    else if(operacion == "resta") {
+        resultado = numero1 - numero2;
+    }
+    else if(operacion == "multiplica") {
+        resultado = numero1 * numero2;
+    }
+    else if(operacion == "divide") {
+        resultado = numero1 / numero2;
+    }
+    else { 
+        alert("Funcionalidad en progreso");
+    }
 
+    let result = document.getElementById("resultado");
+    result.innerHTML = resultado;
 
 }
+
+let butones = document.getElementById("btns");
+butones.addEventListener("click",mostrar);
